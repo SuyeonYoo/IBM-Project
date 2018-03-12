e6def431f0f90e6a9f94216607921caca630192a
danbiyun <danbi066@gmail.com>
2018-03-12 13:30:37 +0900

    055501(윤단비)_maven 빌드 시 에러나는 부분 수정 및 반영

--- src/test/java/com/example/demo/bluewikiApplicationTests.java
+++ src/test/java/com/example/demo/bluewikiApplicationTests.java
@@ -5,9 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bluewiki.bluewikiApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class BoadApplicationTests {
+@SpringBootTest(classes = bluewikiApplication.class)
+public class bluewikiApplicationTests {
 
 	@Test
 	public void contextLoads() {
